83cad4c7f0761c484a7ef415c55e4c605d8037f4
mokrunka <hunker.engineer@gmail.com>
2020-06-05 11:09:29 -0700

    updates to neuralnetworks.py, from video 5

--- neuralnetworks.py
+++ neuralnetworks.py
@@ -3,9 +3,21 @@ import numpy as np
 np.random.seed(0)
 
 # input data (denoted with capital X by convention)
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+# X = [[1, 2, 3, 2.5],
+#      [2.0, 5.0, -1.0, 2.0],
+#      [-1.5, 2.7, 3.3, -0.8]]
+
+# dataset generator
+def create_data(points, classes):
+    X = np.zeros((points * classes, 2))
+    y = np.zeros(points * classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points * class_number, points * (class_number + 1))
+        r = np.linspace(0.0, 1, points)
+        t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points) * 0.2
+        X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
+        y[ix] = class_number
+    return X, y
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -14,13 +26,22 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+    '''rectified linear activation function'''
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 # the n_neurons can be anything, we chose 5
-# the n_inputs is the length of one row of the X input data
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+# the n_inputs is the length of one row of the X input data (like different sensor readings)
+layer1 = Layer_Dense(2, 5)
+
+# create the data set, 3 sets, 100 values per set
+X, y = create_data(100, 3)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-print(layer1.output)
+activation1.forward(layer1.output)
+# print(layer1.output)
+print(activation1.output)
 
-layer2.forward(layer1.output)
-print(layer2.output)
+
